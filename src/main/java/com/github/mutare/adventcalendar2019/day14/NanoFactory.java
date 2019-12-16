7ce261dd58c04ea9f50a3d40cd89d332811fd9dc
mglowacz <mglowacz@omnis.pl>
2019-12-16 15:23:02 +0100

    day 14 two

--- src/main/java/com/github/mutare/adventcalendar2019/day14/NanoFactory.java
+++ src/main/java/com/github/mutare/adventcalendar2019/day14/NanoFactory.java
@@ -1,6 +1,7 @@
 package com.github.mutare.adventcalendar2019.day14;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -10,23 +11,26 @@ import static java.util.Arrays.asList;
 public class NanoFactory {
 
     private final Map<String, Reaction> reactions;
+    private List<Factor> wastes;
 
     public NanoFactory(Map<String, Reaction> reactions) {
         this.reactions = reactions;
     }
 
-    public int findOreQuantity() {
-        Reaction fuel = reactions.get("FUEL");
-
-        List<Factor> factors = new ArrayList<>(fuel.factors);
-        while(reduceFactors(factors, reactions) == 0) {
-            factors = mergeFactors(factors);
-        }
-
-        return factors.stream().map(this::toOre).reduce(Integer::sum).orElseThrow();
+    public long findOreQuantity() {
+        return findOreQuantityFor(reactions.get("FUEL").output);
     }
 
-    private int toOre(Factor factor) {
+    public long findOreQuantityFor(Factor f) {
+        List<Factor> factors = new ArrayList<>(Collections.singletonList(new Factor(f)));
+        while (reduceFactors(factors) == 0) {
+            factors = mergeFactors(factors);
+        }
+        wastes = factors.stream().filter(factor -> !factor.type.equals("ORE")).collect(Collectors.toList());
+        return factors.stream().map(this::toOre).reduce(Double::sum).orElseThrow().longValue();
+    }
+
+    private double toOre(Factor factor) {
         return factor.type.equals("ORE") ?
                 factor.quantity :
                 ((int) (reactions.get(factor.type).factors.get(0).quantity * Math.ceil((double) factor.quantity / reactions.get(factor.type).output.quantity)));
@@ -35,26 +39,49 @@ public class NanoFactory {
     private List<Factor> mergeFactors(List<Factor> factors) {
         return factors.stream()
                 .filter(factor -> factor.quantity != 0)
-                .collect(Collectors.toMap(factor -> factor.type, factor -> factor.quantity, Integer::sum))
+                .collect(Collectors.toMap(factor -> factor.type, factor -> factor.quantity, Long::sum))
                 .entrySet()
                 .stream()
-                .map(stringIntegerEntry -> new Factor(stringIntegerEntry.getKey(), stringIntegerEntry.getValue()))
+                .map(stringLongEntry -> new Factor(stringLongEntry.getKey(), stringLongEntry.getValue()))
                 .collect(Collectors.toList());
     }
 
-    private int reduceFactors(List<Factor> factors, Map<String, Reaction> reactions) {
+    private int reduceFactors(List<Factor> factors) {
         for (Factor factor : factors) {
             if (factor.type.equals("ORE")) {
                 continue;
             }
             Reaction reaction = reactions.get(factor.type);
             if (factor.quantity > 0) {
-                factor.quantity -= reaction.output.quantity;
-                factors.addAll(reaction.factors);
+                long m = (long) Math.ceil((double) factor.quantity / reaction.output.quantity);
+                factor.quantity -= m * reaction.output.quantity;
+                for (Factor f : reaction.factors) {
+                    factors.add(new Factor(f.type, f.quantity * m));
+                }
                 return 0;
             }
         }
         return 1;
+    }
+
+    public long findMaxFuel(long oreLimit) {
+        long orePerFuel = findOreQuantity();
+        long min = oreLimit / orePerFuel;
+        long max = min * 3;
+
+        return searchOreQuantityFor("FUEL", min, max, oreLimit);
+    }
+
+    private long searchOreQuantityFor(String type, long min, long max, long oreLimit) {
+        long avg = (long)Math.ceil((max + min) / 2);
+        long fuel1 = findOreQuantityFor(new Factor(type, avg));
+        if (max - min < 2) {
+            return avg;
+        }
+
+        return fuel1 > oreLimit ?
+                searchOreQuantityFor(type, min, avg, oreLimit) :
+                searchOreQuantityFor(type, avg, max, oreLimit);
     }
 
     static class Reaction {
@@ -63,19 +90,25 @@ public class NanoFactory {
             this.factors = asList(factors);
             this.isOreReaction = this.factors.stream().allMatch(factor -> factor.type.equals("ORE"));
         }
+
         List<Factor> factors;
         Factor output;
         boolean isOreReaction;
     }
 
     static class Factor {
-        Factor(String type, int quantity) {
+        Factor(String type, long quantity) {
             this.type = type;
             this.quantity = quantity;
         }
 
         String type;
-        int quantity;
+        long quantity;
+
+        public Factor(Factor f) {
+            this.quantity = f.quantity;
+            this.type = f.type;
+        }
     }
 
 }
--- src/test/java/com/github/mutare/adventcalendar2019/day14/StoichiometryTest.java
+++ src/test/java/com/github/mutare/adventcalendar2019/day14/StoichiometryTest.java
@@ -1,5 +1,6 @@
 package com.github.mutare.adventcalendar2019.day14;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -10,39 +11,59 @@ public class StoichiometryTest {
 
     @Test
     public void test() throws IOException {
-        int ore = new NanoFactory(new FileReactionsInputSource().getReactions("/day14/test")).findOreQuantity();
+        long ore = new NanoFactory(new FileReactionsInputSource().getReactions("/day14/test")).findOreQuantity();
         assertEquals(31, ore);
     }
 
     @Test
     public void test2() throws IOException {
-        int ore = new NanoFactory(new FileReactionsInputSource().getReactions("/day14/test2")).findOreQuantity();
+        long ore = new NanoFactory(new FileReactionsInputSource().getReactions("/day14/test2")).findOreQuantity();
         assertEquals(165, ore);
     }
 
     @Test
     public void test3() throws IOException {
-        int ore = new NanoFactory(new FileReactionsInputSource().getReactions("/day14/test3")).findOreQuantity();
+        long ore = new NanoFactory(new FileReactionsInputSource().getReactions("/day14/test3")).findOreQuantity();
         assertEquals(13312, ore);
     }
 
     @Test
     public void test4() throws IOException {
-        int ore = new NanoFactory(new FileReactionsInputSource().getReactions("/day14/test4")).findOreQuantity();
+        long ore = new NanoFactory(new FileReactionsInputSource().getReactions("/day14/test4")).findOreQuantity();
         assertEquals(180697, ore);
     }
 
     @Test
     public void test5() throws IOException {
-        int ore = new NanoFactory(new FileReactionsInputSource().getReactions("/day14/test5")).findOreQuantity();
+        long ore = new NanoFactory(new FileReactionsInputSource().getReactions("/day14/test5")).findOreQuantity();
         assertEquals(2210736, ore);
     }
 
     @Test
     public void one() throws IOException {
-        int ore = new NanoFactory(new FileReactionsInputSource().getReactions("/day14/data")).findOreQuantity();
+        long ore = new NanoFactory(new FileReactionsInputSource().getReactions("/day14/data")).findOreQuantity();
         assertEquals(628586, ore);
     }
 
+
+    @Test
+    public void test3_2() throws IOException {
+        assertEquals(82892753, new NanoFactory(new FileReactionsInputSource().getReactions("/day14/test3")).findMaxFuel(1000000000000L));
+    }
+
+    @Test
+    public void test4_2() throws IOException {
+        assertEquals(5586022, new NanoFactory(new FileReactionsInputSource().getReactions("/day14/test4")).findMaxFuel(1000000000000L));
+    }
+
+    @Test
+    public void test5_2() throws IOException {
+        assertEquals(460664, new NanoFactory(new FileReactionsInputSource().getReactions("/day14/test5")).findMaxFuel(1000000000000L));
+    }
+
+    @Test
+    public void two() throws IOException {
+        assertEquals(3209254, new NanoFactory(new FileReactionsInputSource().getReactions("/day14/data")).findMaxFuel(1000000000000L));
+    }
 
 }

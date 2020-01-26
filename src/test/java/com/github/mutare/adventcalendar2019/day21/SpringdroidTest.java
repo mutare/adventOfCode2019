294e3376a06956f58ecda862bdf9f57fdca9d2de
mglowacz <mglowacz@omnis.pl>
2020-01-26 23:38:52 +0100

    day 21 two

--- src/test/java/com/github/mutare/adventcalendar2019/day21/SpringdroidTest.java
+++ src/test/java/com/github/mutare/adventcalendar2019/day21/SpringdroidTest.java
@@ -9,21 +9,42 @@ import static org.junit.Assert.assertEquals;
 
 public class SpringdroidTest {
     @Test
-    public void test() throws IOException {
+    public void one() throws IOException {
         Springdroid springdroid = new Springdroid(new FileProgramInputSource().getProgram("/day21/data"));
         String output = springdroid.output();
-        springdroid.input("NOT C T\n" +
-                "NOT T T\n" +
-                "AND B T\n" +
-                "AND A T\n" +
-                "NOT T T\n" +
-                "AND D T\n" +
-                "NOT T T\n" +
-                "NOT T J\n" +
+        springdroid.input(
+                "NOT C J\n" +
+                        "NOT J J\n" +
+                        "AND B J\n" +
+                        "AND A J\n" +
+                        "NOT J J\n" +
+                        "AND D J\n" +
                 "WALK\n");
 
         output = springdroid.output();
         System.out.println(output);
         assertEquals(19349722, springdroid.getResult());
+    }
+
+    @Test
+    public void two() throws IOException {
+        Springdroid springdroid = new Springdroid(new FileProgramInputSource().getProgram("/day21/data"));
+        String output = springdroid.output();
+        springdroid.input(
+                "NOT H T\n" +
+                        "NOT T T\n" +
+                        "OR E T\n" +
+                        "NOT C J\n" +
+                        "NOT J J\n" +
+                        "AND B J\n" +
+                        "AND A J\n" +
+                        "NOT J J\n" +
+                        "AND D J\n" +
+                        "AND T J\n" +
+                "RUN\n");
+
+        output = springdroid.output();
+        System.out.println(output);
+        assertEquals(1141685254, springdroid.getResult());
     }
 }
